23f7c183ebf0e91f9165d4d73b7320c8455f2172
Tran Ngoc <tranngoc08012007@gmail.com>
2026-06-20 15:14:32 +0700

    print(), comment and variable

---
+++ Phase_01/Lesson_01/main.py
@@ -0,0 +1,23 @@
+# Variable erstellen – Informationen im Speicher speichern.
+name = "Trần Nguyên Ngọc" # Mein Name
+beruf = "Fachinformatiker Anwendungsentwicklung" # Mein Beruf
+geburtsjahr = 2007 # Mein Geburtsjahr
+aktuelles_jahr = 2026 # Das aktuelle Jahr.
+alter = aktuelles_jahr - geburtsjahr # Mein Alter.
+
+# Variablen in print() verwenden.
+print(name) # Name ausgeben.
+print(beruf) # Beruf ausgeben. 
+print(alter) # Alter ausgeben.
+
+# Den Wert einer Variable ändern – das ist völlig erlaubt.
+alter = 20 # Den alten Wert überschreiben.
+print(alter) # und 20 ausgeben.
+
+ausbildungsjahr = 2027 # voraussichtlicher Ausbildungsbeginn.
+stadt = "Ho chi minh stadt" # Meine Stadt. 
+jahre_bis_ausbildung = ausbildungsjahr - aktuelles_jahr # Jahre bis zur Ausbildung
+
+print(name) # Name ausgeben. 
+print(stadt) # stadt ausgeben 
+print(jahre_bis_ausbildung) # Jahre bis zur Ausbildung ausgeben.
